30fc6753b25f728cd1fa6adcb42c217e1543735a
feng <a10880217@163.com>
2024-03-10 10:56:07 +0800

    viewing

--- resources/views/frontend/pages/product_detail.blade.php
+++ resources/views/frontend/pages/product_detail.blade.php
@@ -48,7 +48,12 @@
         <div class="row">
             <div class="col-12">
                 <div class="row">
+                    <div class="col-lg-6 col-12">
+                        {{-- Product Slider --}}
+
                     <div class="product-gallery">
+                        {{-- Images slider --}}
+                        <div class="flexslider-thumbnails">
                         <ul class="slides">
                             @php
                                 $photo = explode(',',$product_detail->photo);
@@ -96,7 +101,7 @@
                             $after_discount = ($prouct_detail->price-(($product_detail->price*$product_detail->discount)/100));
 
                         @endphp
-                        <p> class="price"><span class="diccount">${{number_format($after_discount,2)}}</span><s>${{number_format($product_detail->price,2)}}</s></p>
+                        <p class="price">><span class="diccount">${{number_format($after_discount,2)}}</span><s>${{number_format($product_detail->price,2)}}</s></p>
                         <p class="description">{!! ($product_detail->summary) !!}</p>
 
                     </div>
@@ -134,6 +139,13 @@
                                         <i class="ti-minus"></i>
                                     </button>
                                 </div>
+                                <input type="hidden" name="slug" value="{{$product_detail->slug}}">
+                                <input type="text" name="quant[1]" class="input-number" data-min="1" data-max="1000" value="1" id="quantity">
+                                <div class="button plus">
+                                    <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[1]">
+                                        <i class="ti-plus"></i>
+                                    </button>
+                                </div>
                             </div>
                             {{-- End Input Order --}}
                         </div>
@@ -165,9 +177,12 @@
                     <ul class="nav nav-tabs" id="mytab" role="tablist">
                         <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#description" role="tab">Description</a></li>
                         <li class="nav-item"><a  class="nav-link" data-toggle="tab" href="#review" role="tab">Reviews</a></li>
-
+                       
                     </ul>
+                    {{-- End Tab Nav --}}
                 </div>
+
+
                 <div class="tab-content" id="myTabContent">
                    {{-- Description --}}
 
@@ -182,7 +197,7 @@
                             </div>
                         </div>
                     </div>
-
+                   </div>
                     {{-- End Description --}}
 
 
@@ -258,13 +273,297 @@
                                         @endauth
                                     </div>
                                 </div>
+
+                                  <div class="ratting-main">
+                                    <div class="avg-ratting">
+                                        <h4>{{ceil($product_detail->getReview->avg('rate'))}} <span>(Overall)</span></h4>
+                                        <span>Base on {{$product_tetail->getReivew->count()}} Comments</span>
+
+                                    </div>
+                                    @foreach ($product_tetail['getReview'] as $data)
+
+                                    {{-- Single Rating --}}
+                                        <div class="single-rating">
+                                            <div class="rating-author">
+                                                @if ($data->user_info['photo'])
+                                                    <img src="{{$data->user_info['photo']}}" alt="{{$data->user_info['photo']}}">
+                                                    @else
+                                                    <img src="{{asset('backend/img/avatar.png')}}" alt="Profile.jpg">
+
+                                                @endif
+                                            </div>
+                                            <div class="rating-des">
+                                                <h6>{{$data->user_info['name']}}</h6>
+                                                <div class="rating">
+                                                    <ul class="rating">
+                                                        @for ($i = 1;$i<=5;$i++)
+                                                        @if ($data->rate>=$i)
+                                                        <li><i class="fa fa-star"></i></li>
+                                                            
+                                                        @else
+                                                        <li><i class="fa fa-star-o"></i></li>
+                                                        @endif
+                                                            
+                                                        @endfor
+                                                    </ul>
+                                                    <div class="rate-count">(<span>{{$data->rate}}</span>)</div>
+
+                                                </div>
+                                                <p>{{$data->review}}</p>
+                                            </div>
+                                        </div>
+                                        {{-- End Single Rating --}}
+                                    @endforeach
+
+                                  </div>
+                                  {{-- End Review --}}
+
                             </div>
                         </div>
                     </div>
                    </div>
+                   {{-- End Review Tab --}}
                 </div>
             </div>
         </div>
     </div>
+    </div>
+</div>
+</div>
 </section>
+
+{{-- Eng Shop Single --}}
+
+{{-- Start Most Popular --}}
+
+<div class="product-area most-pupular related-product section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Related Products</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            {{-- {{$product_tetail->rel_prods}} --}}
+            <div class="col-12">
+                <div class="owl-carousel popular-slider">
+                    @foreach ($product_detail->rel_prods as $data)
+                        @if ($data->id !==$product_tetail->id)
+                        {{-- Start Single Product --}}
+                            <div class="single-product">
+                                <div class="prodoct-img">
+                                <a href="{{route('product-detail',$data->slug)}}">
+                                @php
+                                    $photo=explode(',',$data->photo);
+
+                                @endphp
+                                <img  class="default-img" src="{{$photo[0]}}" alt="{{$photo[0]}}">
+                                <img class="hover-img" src="{{$photo[0]}}" alt="{{$photo[0]}}">
+                                <span class="price-dec">{{$data->discount}}% Off</span>
+                                {{-- <span class="out-of-stock">Hot</span> --}}
+                                </a>
+
+                                <div class="button-head">
+                                    <div class="product-action">
+                                        <a data-toggle="modal" data-target="#modelExample" title="Quick View"  href="#"><i class="ti-eye"></i><span>Quick Shop</span></a>
+                                        <a title="Wishlist" href="#"><i class="ti-heart"></i><span>Add to Wishlist</span></a>
+                                        <a title="Compare" href="#"><i class="ti-bar-chart-alt"></i><span>Add to Compare</span></a>
+
+                                    </div>
+                                    <div class="product-action-2">
+                                        <a title="Add to cart" href="#">Add to cart</a>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="product-content">
+                                <h3><a href="{{route('product-detail',$data->slug)}}">{{$data->title}}</a></h3>
+                                <div class="product-price">
+                                    @php
+                                        $after_discount = ($data->price-(($data->discount*$data->price)/100));
+
+                                    @endphp
+                                    <span class="old">${{number_format($data->price,2)}}</span>
+                                    <span>${{number_format($after_discount,2)}}</span>
+
+                                </div>
+                            </div>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- End Most Popular Area --}}
+
+
+{{-- Modal --}}
+<div class="modal fade" id="modelExample" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="ti-close" aria-hidden="true"></span></button>
+
+            </div>
+            <div class="modal-body">
+                <div class="row no-gutters">
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                        {{-- Product Slider --}}
+                        <div class="product-gallery">
+                            <div class="quickview-slider-active">
+                                <div class="single-slider">
+                                    <img src="images/modal.png" alt="#">
+                                </div>
+                                <div class="single-slider">
+                                    <img src="images/modal2.png" alt="#">
+                                </div>
+                                <div class="single-slider">
+                                    <img src="images/modal3.png" alt="">
+                                </div>
+                                <div class="single-slider">
+                                    <img src="images/modal3.png" alt="#">
+                                </div>
+                            </div>
+                        </div>
+                        {{-- End Product slider --}}
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                        <div class="quickview-content">
+                            <h2>Flared Shift Dress</h2>
+                            <div class="quickview-ratting-review">
+                                <div class="quickview-ratting-wrap">
+                                    <div class="quickview-ratting">
+                                        <i class="yellow fa fa-star"></i>
+                                        <i class="yellow fa fa-star"></i>
+                                        <i class="yellow fa fa-star"></i>
+                                        <i class="yellow fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                    <a href="#">(1 customer review)</a>
+
+                                </div>
+                                <div class="quickview-stock">
+                                    <span><i class="fa fa-check-circle-o"></i>in stock</span>
+                                </div>
+                            </div>
+                            <h3>$29.00</h3>
+                            <div class="quickview-peragraph">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia iste laborum ad impedit pariatur esse optio tempora sint ullam autem deleniti nam in quos qui nemo ipsum numquam.</p>
+
+                            </div>
+                            <div class="size">
+                                <div class="row">
+                                    <div class="col-lg-6 col-12">
+                                        <h5 class="title">Size</h5>
+                                        <select>
+                                            <option selected="selected">s</option>
+                                            <option >m</option>
+                                            <option >l</option>
+                                            <option >xl</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-6 col-12">
+                                        <h5 class="title">Color</h5>
+                                        <select>
+                                            <option selected="selected">orange</option>
+                                                <option >purple</option>
+                                                <option >black</option>
+                                                <option >pink</option>
+                                            
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="quantity">
+                                {{-- Input Order --}}
+                                <div class="input-group">
+                                    <button type="button" class="btn btn-primary btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+
+                                        <i class="ti-minus"></i>
+                                    </button>
+                                </div>
+                                <input type="text" name="qty" class="input-number" data-min="1" data-max="1000" value="1">
+                                <div class="button plus">
+                                    <button type="button" class="btn btn-primary btn-number" data-type="plus" data-field="quant[1]">
+                                        <i class="ti-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            {{-- End Input Order --}}
+                            <div class="add-to-cart">
+                                <a href="#" class="btn">Add to cart</a>
+                                <a href="#" class="btn min"><i class="ti-heart"></i></a>
+                                <a href="#" class="btn min"><i class="fa fa-compress"></i></a>
+
+                            </div>
+                            <div class="default-social">
+                                <h4 class="share-now">Share:</h4>
+                                <ul>
+                                    <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a class="youtube" href="#"><i class="fa fa-youtube"></i></a></li>
+                                    <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- Modal end --}}
 @endsection
+
+@push('styles')
+    <style>
+        .rating_box{
+            display: inline-flex;
+        }
+        .star-rating{
+            font-size: 0;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+        .star-rating__wrap{
+            display: inline-block;
+            font-size: 1rem;
+        }
+        .star-rating__wrap::after{
+            content: "";
+            display: table;
+            clear: both;
+        }
+        .rating-rating__ico{
+            float: right;
+            padding-left: 2px;
+            cursor: pointer;
+            color: #f7941d;
+            font-size: 16px;
+            margin-top: 5px;
+        }
+        .star-rating__ico:last-child{
+            padding-left: 0;
+        }
+        .star-rating__input{
+            display: none;
+        }
+        .star-rating__ico:hover::before,
+        .star-rating__ico:hover ~ .star-rating__ico::before,
+        .star-rating__input:checked ~ .star-rating__ico::before{
+            content: "\F005";
+        }
+    </style>
+@endpush
+
+@push('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+    
+@endpush
